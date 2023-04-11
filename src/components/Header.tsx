798d683ae41bf592f598b2fe237eff2773165ea0
Leandro Vicario <leo.vicario@titanfx.com>
2023-04-11 11:16:19 +0800

    Add alt attribute values to images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,11 +18,11 @@ const Header: React.FC = () => {
         // TODO: fix BG color
         <section className="w-full h-24 md:h-20 lg:h-[90px] pl-7 flex gap-x-5 items-center justify-start bg-[#232323] relative group header">
             {isNotDesktop && (
-                <Image src={mobileMenu} className="mobile-menu w-6 h-auto cursor-pointer" width="24" height="19" alt="" onClick={toggleMenu} />
+                <Image src={mobileMenu} className="mobile-menu w-6 h-auto cursor-pointer" width="24" height="19" alt="Mobile Menu" onClick={toggleMenu} />
             )}
             <div className={`flex ${isMobile ? "flex-col gap-1.5 flex-nowrap items-center w-full mr-10" : ""}`}>
                 <Link href="/">
-                    <Image src={logo} className="w-[120px] md:w-[151px] md:h-[36px] md:mr-20" width="120" height="36" alt="" />
+                    <Image src={logo} className="w-[120px] md:w-[151px] md:h-[36px] md:mr-20" width="120" height="36" alt="TitanFx" />
                 </Link>
                 <HeaderPortal />
             </div>
