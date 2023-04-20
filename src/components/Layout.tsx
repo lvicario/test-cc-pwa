b3cc78df99307855bf04d348d8d52e5e2b874789
Leandro Vicario <leo.vicario@titanfx.com>
2023-04-20 12:44:38 +0800

    Add back auth0 provider and redirect to login

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,9 +3,9 @@ import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import HeaderLinks from './HeaderLinks';
-// import { useUser } from '@auth0/nextjs-auth0/client';
-// import { useEffect } from 'react';
-// import { useRouter } from 'next/router';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -13,18 +13,18 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
     const { isDesktop } = useMediaQuery();
-    // const { user, isLoading } = useUser();
-    // const router = useRouter();
+    const { user, isLoading } = useUser();
+    const router = useRouter();
 
-    // useEffect(() => {
-    //   if (!isLoading && !user) {
-    //     router.push('/api/auth/login');
-    //   }
-    // }, [isLoading, user, router]);
+    useEffect(() => {
+      if (!isLoading && !user) {
+        router.push('/api/auth/login');
+      }
+    }, [isLoading, user, router]);
   
-    // if (isLoading || !user) {
-    //   return <p>Loading...</p>;
-    // }
+    if (isLoading || !user) {
+      return <p>Loading...</p>;
+    }
 
     return (
         <>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
 import type { AppProps } from 'next/app';
-// import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 import Layout from '@/components/Layout';
 import { AppProvider } from '@/context/AppContext';
 import '@/styles/app.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        // <UserProvider>
+        <UserProvider>
           <AppProvider>
               <Layout>
                   <Component {...pageProps} />
               </Layout>
           </AppProvider>
-        // </UserProvider>
+        </UserProvider>
     )
 }
